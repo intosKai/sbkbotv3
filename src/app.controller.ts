5cc1700cfe9e1d9d11ebc9107e7cb5084f53c396
Petr Ivanov <petan4ik@gmail.com>
2020-09-17 23:14:24 +0300

    fix body

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor() {}
 
   @Get('vk')
-  async vk(@Body request: any): Promise<string> {
+  async vk(@Body() request: any): Promise<string> {
     console.log('new request', JSON.stringify(request));
     return VK_CALLBACK_ANSWER;
   }
