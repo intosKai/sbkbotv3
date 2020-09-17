8d428e7578c2367a438ee1096f86763c759c7b4f
Petr Ivanov <petan4ik@gmail.com>
2020-09-17 23:11:46 +0300

    log

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import { VK_CALLBACK_ANSWER } from './config';
 
@@ -7,8 +7,8 @@ export class AppController {
   constructor() {}
 
   @Get('vk')
-  async vk(request: any): Promise<string> {
-    console.log(request);
+  async vk(@Body request: any): Promise<string> {
+    console.log('new request', JSON.stringify(request));
     return VK_CALLBACK_ANSWER;
   }
 }
